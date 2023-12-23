fc0338a71076e169691b3eae4f26c7b96769b06c
63vladislav <vladushka6321@icloud.com>
2023-12-23 10:27:48 +0400

    Initial Commit

---
+++ Dz10_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dz10_
+//
+//  Created by 111 on 23.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
